2a6e861f4724b0ee478cee72248d1640933fd085
shengyanli1982 <shengyanlee36@gmail.com>
2024-02-02 14:10:53 +0800

    Improved code remarks and optimized GC optimization of the pop-up node in Queue

--- internal/pool/pool.go
+++ internal/pool/pool.go
@@ -1,18 +1,22 @@
 package pool
 
+// NewFunc is a function type that creates a new instance of an object.
 type NewFunc = func() interface{}
 
+// StackInterface is an interface that defines the behavior of a stack.
 type StackInterface interface {
 	Push(value interface{})
 	Pop() interface{}
 	Len() uint64
 }
 
+// Pool is a struct that represents an object pool.
 type Pool struct {
 	newFunc NewFunc
 	stack   StackInterface
 }
 
+// NewPool creates a new object pool with the given newFunc and stack.
 func NewPool(newFunc NewFunc, stack StackInterface) *Pool {
 	p := Pool{
 		newFunc: newFunc,
@@ -22,6 +26,7 @@ func NewPool(newFunc NewFunc, stack StackInterface) *Pool {
 	return &p
 }
 
+// Get retrieves an object from the pool. If the pool is empty, a new object is created using newFunc.
 func (p *Pool) Get() interface{} {
 	v := p.stack.Pop()
 	if v == nil {
@@ -30,10 +35,12 @@ func (p *Pool) Get() interface{} {
 	return v
 }
 
+// Put puts an object back into the pool.
 func (p *Pool) Put(v interface{}) {
 	p.stack.Push(v)
 }
 
+// Prune removes a portion of the objects from the pool.
 func (p *Pool) Prune() {
 	count := uint64(float64(p.stack.Len()) * 0.33)
 	for i := uint64(0); i < count; i++ {
--- internal/stack/lockfree.go
+++ internal/stack/lockfree.go
@@ -5,58 +5,113 @@ import (
 	"unsafe"
 )
 
+// Element 是栈中的元素
+// Element is an element in the stack
 type Element struct {
-	next  unsafe.Pointer
-	value interface{}
+	next  unsafe.Pointer // 指向下一个元素的指针
+	value interface{}    // 元素的值
 }
 
+// LockFreeStack 是一个无锁栈
+// LockFreeStack is a lock-free stack
 type LockFreeStack struct {
-	top    unsafe.Pointer
-	length uint64
+	top    unsafe.Pointer // 栈顶指针
+	length uint64         // 栈的长度
 }
 
+// NewLockFreeStack 创建一个新的无锁栈
+// NewLockFreeStack creates a new lock-free stack
 func NewLockFreeStack() *LockFreeStack {
 	return &LockFreeStack{
+		// 初始化栈顶指针为空元素
+		// Initialize the top pointer to an empty element
 		top: unsafe.Pointer(&Element{}),
 	}
 }
 
+// Pop 从栈中弹出一个元素
+// Pop pops an element from the stack
 func (s *LockFreeStack) Pop() interface{} {
 	for {
+		// 获取栈顶指针
+		// Get the top pointer
 		top := atomic.LoadPointer(&s.top)
+
+		// 栈为空，返回nil
+		// If the stack is empty, return nil
 		if top == nil {
 			return nil
 		}
+
+		// 将栈顶指针转换为Element类型
+		// Convert the top pointer to Element type
 		item := (*Element)(top)
+
+		// 获取下一个元素的指针
+		// Get the pointer of the next element
 		next := atomic.LoadPointer(&item.next)
+
+		// 将栈顶指针指向下一个元素
+		// Set the top pointer to the next element
 		if atomic.CompareAndSwapPointer(&s.top, top, next) {
+			// 栈长度减1
+			// Decrement the length of the stack
 			atomic.AddUint64(&s.length, ^uint64(0))
+
+			// 获取弹出元素的值
+			// Get the value of the popped element
 			value := item.value
-			item.next = nil // 清除引用，帮助 GC
+
+			// 清除引用，帮助GC
+			// Clear the reference to help GC
+			item.next = nil
 			item.value = nil
+
+			// 返回弹出元素的值
+			// Return the value of the popped element
 			return value
 		}
 	}
 }
 
+// Push 将一个元素压入栈中
+// Push pushes an element into the stack
 func (s *LockFreeStack) Push(value interface{}) {
+	// 创建一个新的元素
+	// Create a new element
 	item := &Element{value: value}
-	var top unsafe.Pointer
+
+	// 无限循环，直到压入元素成功
+	// Infinite loop until the element is pushed successfully
 	for {
-		top = atomic.LoadPointer(&s.top)
+		// 获取栈顶指针
+		// Get the top pointer
+		top := atomic.LoadPointer(&s.top)
+
+		// 将新元素的next指针指向原来的栈顶元素
+		// Set the next pointer of the new element to the original top element
 		item.next = top
+
+		// 将栈顶指针指向新元素
+		// Set the top pointer to the new element
 		if atomic.CompareAndSwapPointer(&s.top, top, unsafe.Pointer(item)) {
+			// 栈长度加1
+			// Increment the length of the stack
 			atomic.AddUint64(&s.length, 1)
 			return
 		}
 	}
 }
 
+// Len 返回栈的长度
+// Len returns the length of the stack
 func (s *LockFreeStack) Len() uint64 {
 	return atomic.LoadUint64(&s.length)
 }
 
+// Reset 重置栈
+// Reset resets
 func (s *LockFreeStack) Reset() {
-	s.top = unsafe.Pointer(&Element{})
-	atomic.StoreUint64(&s.length, 0)
+	s.top = unsafe.Pointer(&Element{}) // 将栈顶指针指向空元素
+	atomic.StoreUint64(&s.length, 0)   // 将栈长度置为0
 }
