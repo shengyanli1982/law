0768ee9b991133b8922de1ee006ba72118c79720
shengyanli1982 <shengyanlee36@gmail.com>
2024-04-28 12:51:49 +0800

    Fixed the OnWriteFailed test method in callback

--- writer_test.go
+++ writer_test.go
@@ -2,22 +2,33 @@ package law
 
 import (
 	"bytes"
-	"os"
+	"errors"
+	"fmt"
 	"testing"
 	"time"
 
-	"github.com/shengyanli1982/law/internal/utils"
 	"github.com/stretchr/testify/assert"
 )
 
+var largeBytes = []byte("wWqt2ZcQmVzk4KZJPmPamr3cBLNEf5dex2N2RkqhS3E7G6PWnzFEukskx5Z822mZd7")
+
 type callback struct {
-	a3 []string
-	e3 []error
+	t *testing.T
 }
 
 func (c *callback) OnWriteFailed(b []byte, err error) {
-	c.a3 = append(c.a3, utils.BytesToString(b))
-	c.e3 = append(c.e3, err)
+	fmt.Printf("## callback.OnWriteFailed(%s, %v)\n", b, err)
+	assert.Equal(c.t, b, largeBytes, "Expected bytes")
+	assert.ErrorIs(c.t, err, errorWriteFailed, "Expected error")
+}
+
+var errorWriteFailed = errors.New("write context failed")
+
+type faultyWriter struct{}
+
+func (fw *faultyWriter) Write(p []byte) (n int, err error) {
+	fmt.Printf("!! faultyWriter.Write(%s)\n", p)
+	return 0, errorWriteFailed
 }
 
 func TestWriteAsyncer_Standard(t *testing.T) {
@@ -77,21 +88,16 @@ func TestWriteAsyncer_EarlyShutdown(t *testing.T) {
 	assert.Equal(t, "helloworld!!!", buff.String())
 }
 
-func TestWriteAsyncer_Callback(t *testing.T) {
-	conf := NewConfig().WithCallback(&callback{})
+func TestWriteAsyncer_OnWriteFailed(t *testing.T) {
+	conf := NewConfig().WithCallback(&callback{t: t}).WithBufferSize(64)
 
-	w := NewWriteAsyncer(os.Stdout, conf)
+	w := NewWriteAsyncer(&faultyWriter{}, conf)
 	defer w.Stop()
 
-	_, err := w.Write([]byte("hello"))
-	assert.Nil(t, err)
-	_, err = w.Write([]byte("world"))
-	assert.Nil(t, err)
-	_, err = w.Write([]byte("!!!"))
-	assert.Nil(t, err)
+	for i := 0; i < 10; i++ {
+		_, err := w.Write(largeBytes)
+		assert.Nil(t, err)
+	}
 
 	time.Sleep(time.Second)
-
-	assert.Equal(t, []string(nil), w.config.callback.(*callback).a3)
-	assert.NoError(t, err, "Expected no error")
 }
