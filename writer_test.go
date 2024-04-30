db10f32f7c07eeb1570ee4dcd9fcaa0acd230ac5
shengyanli1982 <shengyanlee36@gmail.com>
2024-04-30 17:27:29 +0800

    Fix the flushBufferedWriter BUG in the underlying IO error, and improve the corresponding test code.

--- writer_test.go
+++ writer_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var largeBytes = []byte("wWqt2ZcQmVzk4KZJPmPamr3cBLNEf5dex2N2RkqhS3E7G6PWnzFEukskx5Z822mZd7")
+var largeBytes = []byte("#Wqt2ZcQmVzk4KZJPmPamr3cBLNEf5dex2N2RkqhS3E7G6PWnzFEukskx5Z822mZd7")
 
 type callback struct {
 	t *testing.T
@@ -89,15 +89,38 @@ func TestWriteAsyncer_EarlyShutdown(t *testing.T) {
 }
 
 func TestWriteAsyncer_OnWriteFailed(t *testing.T) {
-	conf := NewConfig().WithCallback(&callback{t: t}).WithBufferSize(64)
 
-	w := NewWriteAsyncer(&faultyWriter{}, conf)
-	defer w.Stop()
+	// Will print callback.OnWriteFailed 10 times, faultyWriter.Write 1 time, buff is 66 bytes
+	t.Run("Message large than bufferSize", func(t *testing.T) {
+		conf := NewConfig().WithCallback(&callback{t: t}).WithBufferSize(60)
 
-	for i := 0; i < 10; i++ {
-		_, err := w.Write(largeBytes)
-		assert.Nil(t, err)
-	}
+		w := NewWriteAsyncer(&faultyWriter{}, conf)
+		defer w.Stop()
 
-	time.Sleep(time.Second)
+		for i := 0; i < 10; i++ {
+			_, err := w.Write(largeBytes)
+			assert.Nil(t, err)
+		}
+
+		fmt.Println(">>> Error return by bufio Write method")
+
+		time.Sleep(time.Second)
+	})
+
+	// Will print callback.OnWriteFailed 1 times, faultyWriter.Write 1 time, buff is 594 bytes (Because of the buffer size is 600)
+	t.Run("Message less than bufferSize", func(t *testing.T) {
+		conf := NewConfig().WithCallback(&callback{t: t}).WithBufferSize(600)
+
+		w := NewWriteAsyncer(&faultyWriter{}, conf)
+		defer w.Stop()
+
+		for i := 0; i < 10; i++ {
+			_, err := w.Write(largeBytes)
+			assert.Nil(t, err)
+		}
+
+		fmt.Println(">>> Error return by bufio Flush method")
+
+		time.Sleep(time.Second)
+	})
 }
